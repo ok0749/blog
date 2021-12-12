b3be24499f9f094aeff555b4ae1274c9f811740e
ok0749 <63099945+ok0749@users.noreply.github.com>
2021-12-12 15:01:49 +0900

    글쓴이가 누군지 표시
    mongoose populate 메서드로 author값을 통해 user collection 과 post collection을 join
    join한 값에서 글쓴이 출력

--- controller/index.js
+++ controller/index.js
@@ -2,7 +2,9 @@ const Post = require("../models/posts");
 
 module.exports = {
   getAllPost: async function (req, res) {
-    const posts = await Post.find().sort({ createdAt: "desc" });
+    const posts = await Post.find()
+      .sort({ createdAt: "desc" })
+      .populate("author", "name");
     res.render("index", { posts });
   },
 };
