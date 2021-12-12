888b094d778d31a1554e92f6f4b80569452a60ce
ok0749 <63099945+ok0749@users.noreply.github.com>
2021-12-12 22:24:46 +0900

    savePost, saveEditPost 같은 코드 하나의 함수로 정의
    저장하는 부분 router에 미들웨어 추가, 하나의 함수로 정의해 전보다 코드 깔끔하게 정리

--- controller/posts.js
+++ controller/posts.js
@@ -1,24 +1,27 @@
 const Post = require("../models/posts");
 
-module.exports = {
-  newPage: function (req, res) {
-    res.render("new", { post: new Post() });
-  },
-  savePost: async function (req, res) {
+function savePostAndRedirect(path) {
+  return async (req, res) => {
     const { title, description, markdown } = req.body;
-    let post = new Post({
-      title,
-      description,
-      markdown,
-      author: req.session.user.id,
-    });
+    let post = req.post;
+    post.title = title;
+    post.description = description;
+    post.markdown = markdown;
+    post.author = req.session.user.id;
     try {
       post = await post.save();
       res.redirect(`/posts/${post.id}`);
     } catch (error) {
-      console.error(error);
+      res.render(`posts/${path}`, { post });
     }
+  };
+}
+
+module.exports = {
+  newPage: function (req, res) {
+    res.render("new", { post: new Post() });
   },
+  savePost: savePostAndRedirect("new"),
 
   postPage: async function (req, res) {
     const id = req.params.id;
@@ -33,19 +36,7 @@ module.exports = {
     res.render("post", { post });
   },
 
-  saveEditPost: async function (req, res) {
-    let post = await Post.findById(req.params.id);
-    post.title = req.body.title;
-    post.description = req.body.description;
-    post.markdown = req.body.markdown;
-    post.author;
-    try {
-      post = await post.save();
-      res.redirect(`/posts/${post.id}`);
-    } catch (error) {
-      console.error(error);
-    }
-  },
+  saveEditPost: savePostAndRedirect("edit"),
 
   deletePost: async function (req, res) {
     const post = await Post.findById(req.params.id).populate("author");
