a001febcb5eae2c46b616de4dcae6a0765db3e93
ok0749 <63099945+ok0749@users.noreply.github.com>
2021-12-12 15:03:03 +0900

    siteName 변수 삭제

--- middlewares.js
+++ middlewares.js
@@ -1,5 +1,4 @@
 const localsMiddleware = (req, res, next) => {
-  res.locals.siteName = "My blog";
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.user = req.session.user;
   next();
